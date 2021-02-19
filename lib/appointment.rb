84aaf05e6d3a33f792c4752060a2a515f7851f07
Miguel Angel Morales <mmorales21500@live.com>
2021-02-19 15:48:20 -0500

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,27 @@
-
+require 'pry'
+class Appointment
+    attr_accessor :patient, :doctor, :date 
+  
+    @@all = []
+  
+    def initialize(date, patient, doctor)
+    
+      @patient = patient
+      @doctor = doctor
+      @date = date
+      @@all << self 
+    end 
+  
+    def self.all 
+      @@all 
+    end   
+  
+    def patient
+      @patient
+    end 
+  
+    def doctor
+      @doctor
+    end   
+  
+  end    
